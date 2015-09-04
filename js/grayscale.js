24360aaa43f39ade374cb5644294abd90b4ed5a3
Shreyes Joshi <shreyes.joshi@gmail.com>
2015-09-04 18:32:21 -0400

    project updates

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,7 +6,7 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 175) {
         $('#home-name').text('Shreyes Joshi');
         $(".navbar-fixed-top").addClass("top-nav-collapse");
 
