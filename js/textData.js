f0aafc3e63a7e2849e39256ae172e7626a2f806f
Shreyes Joshi <shreyesjoshi@Shreyess-MacBook-Pro.local>
2016-08-15 23:59:12 -0400

    text json data sourcing

---
+++ js/textData.js
@@ -0,0 +1,54 @@
+var data = {
+  "aboutMe": {
+    "intro": "blah",
+    "middle": "blah",
+    "end": "blah"
+  },
+  "software": {
+    "stocks": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf",
+      "p4": "sdfs"
+    },
+    "ele488": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    },
+    "ele381": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    },
+    "USPTO": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    }
+  },
+  "hardware": {
+    "carlab": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    },
+    "BB8": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    },
+    "ECG": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    }
+  },
+  "other": {
+    "garden": {
+      "p1": "sdf",
+      "p2": "sdfs",
+      "p3": "sdf"
+    }
+  }
+};
